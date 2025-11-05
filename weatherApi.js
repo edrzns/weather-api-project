c6545172a451b6a39c74444288cc9527a0ca1111
CERZ <edijs.rozens@bite.lv>
2025-11-05 12:18:49 +0200

    Refactor by adding async/await

--- weatherApi.js
+++ weatherApi.js
@@ -9,17 +9,21 @@ const weatherApiKey = process.env.WEATHER_API_KEY;
 const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric`;
 const weatherApiUrl = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}`;
 
-const openWeatherPromise = fetch(openWeatherUrl).then(res => res.json());
-const weatherApiPromise = fetch(weatherApiUrl).then(res => res.json());
+const openWeatherPromise = fetch(openWeatherUrl).then((res) => res.json());
+const weatherApiPromise = fetch(weatherApiUrl).then((res) => res.json());
 
-Promise.all([openWeatherPromise, weatherApiPromise])
-  .then(data => {
-    const openWeatherData = data[0];
-    const weatherApiData = data[1];
+async function getWeather() {
+  try {
+    const [openWeatherData, weatherApiData] = await Promise.all([
+      openWeatherPromise,
+      weatherApiPromise,
+    ]);
 
     console.log('OpenWeather Data:', openWeatherData);
     console.log('WeatherAPI Data:', weatherApiData);
-  })
-  .catch(error => {
+  } catch (error) {
     console.error('Error fetching weather data:', error);
-  });
+  }
+}
+
+getWeather();
