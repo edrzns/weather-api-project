33dc92808f4891352814767a2da84b572cc28828
CERZ <edijs.rozens@bite.lv>
2025-11-05 12:37:21 +0200

    Add logging which API responded faster

--- weatherApi.js
+++ weatherApi.js
@@ -9,18 +9,52 @@ const weatherApiKey = process.env.WEATHER_API_KEY;
 const openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric`;
 const weatherApiUrl = `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}`;
 
-const openWeatherPromise = fetch(openWeatherUrl).then((res) => res.json());
-const weatherApiPromise = fetch(weatherApiUrl).then((res) => res.json());
+const openWeatherPromise = timePromise(
+  fetch(openWeatherUrl).then((res) => res.json()),
+  'OpenWeather'
+);
+const weatherApiPromise = timePromise(
+  fetch(weatherApiUrl).then((res) => res.json()),
+  'WeatherAPI'
+);
+
+function timePromise(promise, name) {
+  const startTime = Date.now();
+
+  return promise.then((data) => {
+    const duration = Date.now() - startTime;
+    return {
+      data,
+      duration,
+      name,
+    };
+  });
+}
 
 async function getWeather() {
   try {
-    const [openWeatherData, weatherApiData] = await Promise.all([
+    const [openWeatherResult, weatherApiResult] = await Promise.all([
       openWeatherPromise,
       weatherApiPromise,
     ]);
 
+    const openWeatherData = openWeatherResult.data;
+    const weatherApiData = weatherApiResult.data;
+
     console.log('OpenWeather Data:', openWeatherData);
     console.log('WeatherAPI Data:', weatherApiData);
+
+    if (openWeatherResult.duration < weatherApiResult.duration) {
+      console.log(
+        `OpenWeather was faster: ${openWeatherResult.duration}ms vs ${weatherApiResult.duration}ms`
+      );
+    } else if (weatherApiResult.duration < openWeatherResult.duration) {
+      console.log(
+        `WeatherAPI was faster: ${weatherApiResult.duration}ms vs ${openWeatherResult.duration}ms`
+      );
+    } else {
+      console.log(`Both APIs responded in the same time: ${openWeatherResult.duration}ms`);
+    }
   } catch (error) {
     console.error('Error fetching weather data:', error);
   }
